c9623205fc24263a52b9091e8fe5dadee6e4eaf5
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-15 12:31:47 +0300

    1st task is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('div.gallery');
+
+const imagesGrid = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}><img class= "gallery__image" src=${preview} alt="${description}" width = 340></a>`;
+  })
+  .join('');
+galleryList.innerHTML = imagesGrid;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
