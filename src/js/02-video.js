d2d65e60202bc7e5b600f6ae8ebcedf23b3c3a73
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-16 20:13:55 +0300

    2nd task is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import videoplayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new videoplayer(iframe);
+const CURRENT_TIME_STORAGE_KEY = 'videoplayer-current-time';
+
+const getCurrentTime = player.on(
+  'timeupdate',
+  throttle(function () {
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        return localStorage.setItem(CURRENT_TIME_STORAGE_KEY, seconds);
+      })
+      .catch(function (error) {});
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem(CURRENT_TIME_STORAGE_KEY);
+
+const setCurrentTime = player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
